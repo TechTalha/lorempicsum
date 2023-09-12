5e2d229d041abec6be22ae099311ff2c3fa6b4ec
Talha <101164152+TechTalha@users.noreply.github.com>
2023-09-12 10:05:22 +0500

    little change

--- app/page.js
+++ app/page.js
@@ -35,7 +35,7 @@ const page = () => {
     <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
       <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Lorem Picsum </h2>
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Lorem Picsum Api </h2>
           <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Fetching Lorem Picsum Api and Generating random numbers using Axios in Next Js Project</p>
       </div> 
       <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
